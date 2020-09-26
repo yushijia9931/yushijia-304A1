bb4291a440e90e29014f2c744a484c5bbc8e25c8
yushijia9931 <shijia.yu@mail.utoronto.ca>
2020-09-26 02:06:59 +0000

    Third comment

---
+++ content/post/set-up.R
@@ -0,0 +1,15 @@
+install.packages("tidyverse")
+install.packages("blogdown")
+library("blogdown")
+?blogdown
+blogdown::new_site()
+dir = ".",
+install_hugo = TRUE,
+format = "toml",
+sample = TRUE,
+theme = "yihui/hugo-lithium",
+hostname = "github.com",
+theme_example = TRUE,
+empty_dirs = FALSE,
+to_yaml = TRUE,
+serve = interactive()
