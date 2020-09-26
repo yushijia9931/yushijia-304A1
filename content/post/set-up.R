efaf95513c9775c8677e5029182f45f669540f19
yushijia9931 <shijia.yu@mail.utoronto.ca>
2020-09-26 06:26:46 +0000

    new1

--- content/post/set-up.R
+++ content/post/set-up.R
@@ -2,14 +2,14 @@ install.packages("tidyverse")
 install.packages("blogdown")
 library("blogdown")
 ?blogdown
-blogdown::new_site()
-dir = ".",
-install_hugo = TRUE,
-format = "toml",
-sample = TRUE,
-theme = "yihui/hugo-lithium",
-hostname = "github.com",
-theme_example = TRUE,
-empty_dirs = FALSE,
-to_yaml = TRUE,
-serve = interactive()
+blogdown::new_site(dir = ".",
+                   install_hugo = TRUE,
+                   format = "toml",
+                   sample = TRUE,
+                   theme = "yihui/hugo-lithium",
+                   hostname = "github.com",
+                   theme_example = TRUE,
+                   empty_dirs = FALSE,
+                   to_yaml = TRUE,
+                   serve = interactive())
+rmarkdown::render_site()
